c847f037a82659815afce7d09d9385a6dff0fb9b
NikolayPasyuk <kolyan-1812@mail.ru>
2022-12-20 00:04:40 +0300

    Max Headroom and JavaScript style

---
+++ tasks_from_101-150/123.js
@@ -0,0 +1,15 @@
+function getMax1() {
+    const max =
+        {
+            name: 'Max Headroom'
+        }
+    return max;
+}
+
+function getMax2() {
+    const name =
+        {
+            name: 'Max Headroom'
+        }
+    return name
+}
