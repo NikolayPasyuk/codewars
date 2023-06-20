04b2d3dbbae057909a967ecd099c406f77e03035
NikolayPasyuk <kolyan-1812@mail.ru>
2023-06-20 18:28:36 +0300

    Add Length

---
+++ tasks_from_101-150/150.js
@@ -0,0 +1,9 @@
+function addLength(str) {
+    const words = str.split(' ');
+    const result = [];
+
+    for (let i = 0; i < words.length; i++) {
+        result.push(words[i] + ' ' + words[i].length);
+    }
+    return result;
+}
