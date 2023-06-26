f6eaa5a4fdaab65efc02aef0f68adac301ce0a05
NikolayPasyuk <kolyan-1812@mail.ru>
2023-06-26 14:59:31 +0300

    Grasshopper - Create the rooms

---
+++ tasks_from_151-200/152.js
@@ -0,0 +1,17 @@
+const rooms = {
+    room1: {
+        name: "Room 1",
+        description: "You are in room 1. It's dark and cool. You see a table, a chair, and a door.",
+        isComplete: false
+    },
+    room2: {
+        name: "Room 2",
+        description: "You are in room 2. It's bright and warm. You see a sofa, a bookcase and a window.",
+        isComplete: false
+    },
+    room3: {
+        name: "Room 3",
+        description: "You are in room 3. It's dirty and damp. You see a bed, a closet and a mirror.",
+        isComplete: false
+    }
+};
