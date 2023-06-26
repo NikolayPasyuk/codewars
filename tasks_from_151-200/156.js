6c8da23f5149693ee90eb90d790935d08b8aad1b
NikolayPasyuk <kolyan-1812@mail.ru>
2023-06-26 15:32:28 +0300

    Thinkful - Number Drills: Blue and red marbles

---
+++ tasks_from_151-200/156.js
@@ -0,0 +1,8 @@
+function guessBlue(blueStart, redStart, bluePulled, redPulled) {
+    let blueLeft = blueStart - bluePulled;
+    let redLeft = redStart - redPulled;
+
+    let totalLeft = blueLeft + redLeft;
+
+    return blueLeft / totalLeft;
+}
