efa4bbdaf9038f78c51bea4e35ffdd9bf47d88b8
NikolayPasyuk <kolyan-1812@mail.ru>
2023-06-26 15:09:52 +0300

    Training JS #9: loop statement --while and do..while

---
+++ tasks_from_151-200/153.js
@@ -0,0 +1,14 @@
+function padIt(str, n) {
+    let result = str;
+    let i = 0;
+
+    while (i < n) {
+        if (i % 2 === 0) {
+            result = "*" + result;
+        } else {
+            result = result + "*";
+        }
+        i++;
+    }
+    return result;
+}
