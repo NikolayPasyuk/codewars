c38253e11c0c12efe5d84c7e031affa8d949a056
NikolayPasyuk <kolyan-1812@mail.ru>
2023-07-16 16:06:33 +0300

    Training JS #14: Methods of Number object--toString() and toLocaleString()

---
+++ tasks_from_151-200/159.js
@@ -0,0 +1,6 @@
+function colorOf(r, g, b) {
+    const red = r.toString(16).padStart(2, '0');
+    const green = g.toString(16).padStart(2, '0');
+    const blue = b.toString(16).padStart(2, '0');
+    return `#${red}${green}${blue}`;
+}
