86e080a88b6d72c8b342e56e56c7728d02eb06f7
NikolayPasyuk <kolyan-1812@mail.ru>
2023-07-16 16:39:21 +0300

    Up and down, the string grows

--- tasks_from_151-200/161.js
+++ tasks_from_151-200/161.js
@@ -1,11 +1,1 @@
-function firstToLast(str, c) {
-    const firstIndex = str.indexOf(c);
-    const lastIndex = str.lastIndexOf(c);
-    if (firstIndex === -1) {
-        return -1;
-    } else if (firstIndex === lastIndex) {
-        return 0;
-    } else {
-        return lastIndex - firstIndex;
-    }
-}
+const STRANGE_STRING = "ſß";
