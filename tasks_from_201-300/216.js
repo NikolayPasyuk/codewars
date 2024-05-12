117cf090e04bcd88a4fe0459461a9b6fb73d27eb
NikolayPasyuk <kolyan-1812@mail.ru>
2024-05-12 17:34:32 +0300

    Don't give me five!

---
+++ tasks_from_201-300/216.js
@@ -0,0 +1,9 @@
+const dontGiveMeFive = (start, end) => {
+  let count = 0;
+  for (let i = start; i <= end; i++) {
+    if (String(i).indexOf('5') === -1) {
+      count++;
+    }
+  }
+  return count;
+};
