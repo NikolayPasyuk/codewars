a5ae8a5481bfee708c054d8bc8e1b70afc8db62a
NikolayPasyuk <kolyan-1812@mail.ru>
2024-06-10 21:55:08 +0300

    feat: added task  CompoundArray

---
+++ tasks_from_201-300/236.js
@@ -0,0 +1,15 @@
+const compoundArray = (a, b) => {
+  let result = [];
+  let maxLength = Math.max(a.length, b.length);
+
+  for (let i = 0; i < maxLength; i++) {
+    if (i < a.length) {
+      result.push(a[i]);
+    }
+    if (i < b.length) {
+      result.push(b[i]);
+    }
+  }
+
+  return result;
+};
