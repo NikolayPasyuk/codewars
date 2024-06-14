e7147b05f3eb377f8ae620813dfa7581a5c8807f
NikolayPasyuk <kolyan-1812@mail.ru>
2024-06-14 18:56:14 +0300

    feat: added task 'Coding Meetup #2'

---
+++ tasks_from_201-250/242.js
@@ -0,0 +1,7 @@
+const greetDevelopers = (list) => {
+  list.forEach(function (developer) {
+    developer.greeting = `Hi ${developer.firstName}, what do you like the most about ${developer.language}?`;
+  });
+
+  return list;
+}
